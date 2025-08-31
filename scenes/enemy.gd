f8b2f61d67da95c2c419e99b66997d10f853ff2b
CybernetCatgirl <CybernetCatgirl@users.noreply.github.com>
2025-08-31 12:27:46 -0400

    main scene creation + enemy type enum
    Set up the main scene. Moved camera off of player; will be creating unique scene for camera. Added enum for enemy type to enemies (defaults to stationary).

--- scenes/enemy.gd
+++ scenes/enemy.gd
@@ -1,10 +1,19 @@
 extends RigidBody2D
 
-# Simple variable to keep me from having to do too much effort
-# Easier to use a switch statement for movement than make subclasses
-# 0 = stationary, 1 = horizontal movement
-# 2 = vertical movement, 3 = circular movement?
-var enemy_type = 0 
+enum EnemyType{NO_MOVE, HORI_MOVE, VERT_MOVE, CIRCLE_MOVE}
+
+@export var enemy_type: EnemyType = EnemyType.NO_MOVE
+
+func _ready():
+	var mob_types = Array($EnemySprite.sprite_frames.get_animation_names())
+	
+	#Determine animation type by enemy_type enum.
+	if enemy_type == EnemyType.VERT_MOVE:
+		$EnemySprite.animation = "vertical"
+	else:
+		$EnemySprite.animation = "stationary"
+	
+	$EnemySprite.play()
 
 func _on_visible_on_screen_notifier_2d_screen_exited():
 	queue_free() #TODO: set this to a timer and/or distance variable
--- scenes/main.gd
+++ scenes/main.gd
@@ -4,3 +4,6 @@ extends Node2D
 @export var horizontal_mob: PackedScene
 @export var vertical_mob: PackedScene
 @export var circular_mob: PackedScene
+
+func _ready():
+	$Player.position = $StartPosition.position
